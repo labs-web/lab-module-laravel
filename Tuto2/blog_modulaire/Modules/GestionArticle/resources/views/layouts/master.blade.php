67a295d96487019927bf9361e43d25d50c2d1b59
ESSARRAJ <essarraj.fouad@gmail.com>
2024-11-12 12:33:47 +0100

    save

--- Tuto2/blog_modulaire/Modules/GestionArticle/resources/views/layouts/master.blade.php
+++ Tuto2/blog_modulaire/Modules/GestionArticle/resources/views/layouts/master.blade.php
@@ -17,13 +17,11 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    {{-- Vite CSS --}}
-    {{-- {{ module_vite('build-gestionarticle', 'resources/assets/sass/app.scss') }} --}}
+    @vite('resources/assets/sass/app.scss') 
 </head>
 
 <body>
     @yield('content')
 
-    {{-- Vite JS --}}
-    {{-- {{ module_vite('build-gestionarticle', 'resources/assets/js/app.js') }} --}}
+    @module_vite('build-gestionarticle', 'resources/assets/js/app.js')
 </body>
